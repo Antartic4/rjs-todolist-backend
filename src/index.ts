7f360411fd2fbe58bf583add9097853af5eb6fd0
Antartic4 <robjose1996@hotmail.com>
2024-12-22 16:53:53 -0400

    Hot Fix: Cors

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,15 @@ import taskRoutes from './routes/tasks';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://rjs-todolist-frontend-production.up.railway.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 app.use(express.json());
 app.use('/tasks', taskRoutes);
 
